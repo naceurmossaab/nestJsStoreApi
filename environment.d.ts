67f16ce9f93c2309ffd38683c53be1f69c8ed443
naceurmossaab <naceurmossaab@gmail.com>
2025-02-17 20:01:03 +0100

    initial server config

---
+++ environment.d.ts
@@ -0,0 +1,16 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: number;
+      DB_HOST: string;
+      DB_PORT: number;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+      JWT_SECRET: string;
+      JWT_EXPIRES: string;
+    }
+  }
+}
+
+export { }
